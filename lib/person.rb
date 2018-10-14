5892ea2ff171d76588f288356a5e26c4eb6ad0ea
Robin Tram <robin.tram@gmail.com>
2018-10-14 21:49:20 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -2,12 +2,10 @@ require 'pry'
 
 class Person
   
+  attr_accessor:name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(argument_hash)
-
     argument_hash.each {|key, value| self.send(("#{key}="), value)}
-
-    # binding.pry
   end 
   
 end
