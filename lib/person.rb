36e510bcabb50aa2650142e8404b1ba3add5bc75
Robin Tram <robin.tram@gmail.com>
2018-10-14 21:48:01 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,19 @@ require 'pry'
 
 class Person
   
-  attr_accessor
+  attr_accessor:name=>"Avi",
+ :birthday=>"01/29/1984",
+ :hair_color=>"brown",
+ :eye_color=>"brown",
+ :height=>"short",
+ :weight=>"good",
+ :handed=>"lefty",
+ :complexion=>"decent",
+ :t_shirt_size=>"medium",
+ :wrist_size=>"small",
+ :glove_size=>"normal",
+ :pant_length
+ :pant_width
   
   def initialize(argument_hash)
     binding.pry
