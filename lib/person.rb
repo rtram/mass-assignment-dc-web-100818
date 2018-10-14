9a0fe5fea256218aed16f4cda8449d01dcc79956
Robin Tram <robin.tram@gmail.com>
2018-10-14 21:49:02 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ class Person
   attr_accessor:name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(argument_hash)
-    binding.pry
+    # binding.pry
     argument_hash.each {|key, value| self.send(("#{key}="), value)}
 
 
